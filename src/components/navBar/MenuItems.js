b9e520d4ff9a85cffea387a503e832fc7ced4ee4
jramirezval <jhoan_pinch@hotmail.com>
2021-05-30 16:12:44 -0300

    second

---
+++ src/components/navBar/MenuItems.js
@@ -0,0 +1,34 @@
+export const MenuItems=
+
+[
+{
+    title: 'INICIO',
+    url:'#',
+    cName: 'nav-links'
+},
+
+{
+    title: 'SERVICIOS',
+    url:'#',
+    cName: 'nav-links'
+    
+},
+
+    {
+    title: 'PRODUCTOS',
+    url:'#',
+    cName: 'nav-links'
+    
+},
+
+{
+    title: 'CONTACTO',
+    url:'#',
+    cName: 'nav-links'
+    
+},
+
+
+
+
+]
